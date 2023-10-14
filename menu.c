42895e28907a0b2f7446e43f2845342f1de9cc2c
Taurus85910 <96919923@mail.ru>
2023-10-14 14:43:33 +0300

    1

--- menu.c
+++ menu.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
-#include"max.h"
-#include"min.h"
-#include"diff.h"
-#include"sum.h"
+#include"input_data.h"
+#include"print_result.h"
 
 #define MAX_ARR_SIZE 100
 
-void input_data(int* arr, int* arr_size, int* operation_number);
-void print_result(int* arr, int arr_size, int operation_number);
 
 
 int main()
@@ -22,34 +18,3 @@ int main()
 }
 
 
-
-void input_data(int* arr, int* arr_size, int* operation_number)
-{
-    scanf("%d", operation_number);
-
-    while (getchar() != '\n')
-    {
-        scanf("%d", &arr[(*arr_size)++]);
-    }
-}
-
-
-
-
-void print_result(int* arr, int arr_size, int operation_number)
-{
-    switch (operation_number)
-    {
-    case 0: printf("%d\n", max(arr, arr_size));
-        break;
-    case 1: printf("%d\n", min(arr, arr_size));
-        break;
-    case 2: printf("%d\n", diff(arr, arr_size));
-        break;
-    case 3: printf("%d\n", sum(arr, arr_size));
-        break;
-    default:printf("Данные некорректны");
-
-    }
-  
-}
---
+++ print_result.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include"print_result.h"
+#include"max.h"
+#include"min.h"
+#include"diff.h"
+#include"sum.h"
+
+void print_result(int* arr, int arr_size, int operation_number)
+{
+    switch (operation_number)
+    {
+    case 0: printf("%d\n", max(arr, arr_size));
+        break;
+    case 1: printf("%d\n", min(arr, arr_size));
+        break;
+    case 2: printf("%d\n", diff(arr, arr_size));
+        break;
+    case 3: printf("%d\n", sum(arr, arr_size));
+        break;
+    default:printf("Данные некорректны");
+
+    }
+
+}

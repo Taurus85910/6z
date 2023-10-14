f13289dd2b9028e8d1bab91a1f7c20c47db8035e
Taurus85910 <96919923@mail.ru>
2023-10-14 12:46:34 +0300

    1

--- main.c
+++ main.c
@@ -1,14 +1,55 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-#include"get_name.h"
-#include"print_str.h"
+#include <stdio.h>
+#include"max.h"
+#include"min.h"
+#include"diff.h"
+#include"sum.h"
 
-int main() {
-    char hello[90] = "Hello, ";
-    char* result;
-    result = get_name();
-    print_str(strncat(hello, result, 80));
-    free(result);
+#define MAX_ARR_SIZE 100
+
+void input_data(int* arr, int* arr_size, int* operation_number);
+void print_result(int* arr, int arr_size, int operation_number);
+
+
+int main()
+{
+    int array[MAX_ARR_SIZE];
+    int array_size = 0;
+    int operation_number;
+
+    input_data(array, &array_size, &operation_number);
+    print_result(array, array_size, operation_number);
+
+}
+
+
+
+void input_data(int* arr, int* arr_size, int* operation_number)
+{
+    scanf_s("%d", operation_number);
+
+    while (getchar() != '\n')
+    {
+        scanf_s("%d", &arr[(*arr_size)++]);
+    }
+}
+
+
+
+
+void print_result(int* arr, int arr_size, int operation_number)
+{
+    switch (operation_number)
+    {
+    case 0: printf("%d", max(arr, arr_size));
+        break;
+    case 1: printf("%d", min(arr, arr_size));
+        break;
+    case 2: printf("%d", diff(arr, arr_size));
+        break;
+    case 3: printf("%d", sum(arr, arr_size));
+        break;
+    default:printf("Данные некорректны");
+
+    }
     return 0;
 }
